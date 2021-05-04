69b08036acc7722d4a2af07ee4e1d9e3e5369edc
Irina Zhigalova <Irina.Zhigalova@noveogroup.com>
2021-05-04 21:40:59 +0300

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f85d5b559fbec62568d58612511c7058'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17275337-d4b9-439e-879f-467b560f5f40'
+};
